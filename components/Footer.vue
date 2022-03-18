5fa18ef52bd384a39e16d3a26b6a62e3123fcf6f
Roby Hartono <robyhartonodev312@gmail.com>
2022-03-18 15:04:44 +0100

    Implemented footers sections

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-72 bg-gray-800 px-32 p-16 flex text-white">
+  <div class="w-full h-full bg-gray-800 px-32 p-16 flex text-gray-100 space-x-10">
     <div class="flex flex-col w-1/3 space-y-10">
       <div class="text-2xl font-bold flex space-x-2 items-center">
         <img src="~/assets/images/wakanda-logo-white.svg" alt="logo-wakanda-white" class="h-10 w-10">
@@ -23,17 +23,31 @@
         Copyright &#169; 2022 Wakanda. All rights reserved
       </div>
     </div>
-    <!-- <div class="flex w-2/3">
-      <div class="flex flex-col">
-        <div>cokc yep</div>
+    <div class="flex w-2/3 space-x-16">
+      <div class="flex flex-col space-y-5">
+        <div class="text-xl font-semibold">
+          Company
+        </div>
+        <a href="#">About us</a>
+        <a href="#">Blog</a>
+        <a href="#">Contact us</a>
+        <a href="#">How it works</a>
+        <a href="#">For freelancer</a>
       </div>
-      <div class="flex flex-col">
-        <div>cokc yep</div>
+      <div class="flex flex-col space-y-5">
+        <div class="text-xl font-semibold">
+          Support
+        </div>
+        <a href="#">Help center</a>
+        <a href="#">Terms of service</a>
+        <a href="#">Legal</a>
+        <a href="#">Privacy policy</a>
+        <a href="#">Status</a>
       </div>
-      <div class="flex flex-col">
+      <!-- <div class="flex flex-col">
         <div>cokc yep</div>
-      </div>
-    </div> -->
+      </div> -->
+    </div>
   </div>
 </template>
 
